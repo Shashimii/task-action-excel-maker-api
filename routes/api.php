5c5a88d9e54357ff03526867b64262771a8aefa8
Shashimii <besasamreginald36@gmail.com>
2025-01-14 16:32:51 +0800

    + api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AssginedController;
 use App\Http\Controllers\DutyController;
+use App\Http\Controllers\OfficerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// fetch data
 Route::get('/fetchDuties', [DutyController::class, 'fetchAll']);
+Route::get('/fetchOfficers', [OfficerController::class, 'fetchAll']);
+Route::get('/fetchAssigned', [AssginedController::class, 'fetchAll']);
 
+// post data
+Route::post('/addAssigned', [AssginedController::class, 'storeAssigned']);
 
